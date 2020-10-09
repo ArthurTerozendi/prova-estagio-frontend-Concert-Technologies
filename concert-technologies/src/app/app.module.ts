8a2de2a61542b7d95ad9efffd28f8b238613f711
Arthur Terozendi <arthurterozendi@gmail.com>
2020-10-09 17:52:27 -0300

    começando a trabalhar na pag de login

--- concert-technologies/src/app/app.module.ts
+++ concert-technologies/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +17,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  window.location.href = 'login.html'
+}
