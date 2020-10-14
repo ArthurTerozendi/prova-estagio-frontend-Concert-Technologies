d73c0e0e944e1600c7c15dce6a6505558019aa17
Arthur Terozendi <arthurterozendi@gmail.com>
2020-10-14 15:36:05 -0300

    finalizado navbar

--- concert-technologies/src/app/clientes/clientes-routing.module.ts
+++ concert-technologies/src/app/clientes/clientes-routing.module.ts
@@ -3,10 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ClientesEmpresasModule } from './clientes-empresas/clientes-empresas.module';
 import { ClientesPessoasModule } from './clientes-pessoas/clientes-pessoas.module';
-import { ClientesComponent } from './clientes.component';
 
 const routes: Routes = [
-  {path: '', component: ClientesComponent},
   {path: 'pessoas', loadChildren: () => import('./clientes-pessoas/clientes-pessoas.module').then(m =>ClientesPessoasModule)},
   {path: 'empresas', loadChildren: () => import('./clientes-empresas/clientes-empresas.module').then(m => ClientesEmpresasModule)}
 ];
